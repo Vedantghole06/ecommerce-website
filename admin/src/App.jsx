c84c1b6c4931ec7e44f67659ad1a623a11b9df32
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-04-23 00:26:28 +0530

    backend errors

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css"
 const App = () => {
-  const url = "https://ecom-sv-backend.vercel.app/";
+  const url = "https://ecom-sv-admin.vercel.app/add";
 
   return (
     <>
